6853a666f2cec1b25abf2aaba264374bf264d1a6
Butini <marcbiedmabutier@gmail.com>
2021-07-24 12:26:38 +0200

    Init project

---
+++ ClientRentaProject.Presentation.WinSite/ClientRenta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientRentaProject.Presentation.WinSite
+{
+    public partial class ClientRenta : Form
+    {
+        public ClientRenta()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
